c769a38c89660715c124da9e22a151a4abcc1eb3
Quentin Astié <hello@quentinastie.space>
2024-06-10 01:37:10 +0200

    test sans modes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Home } from "./pages/Home.jsx";
 import { AnimatedRoute } from "./AnimatedRoute.jsx";
 import "./css/App.css";
-import "./css/modes/morning.css";
-import "./css/modes/afternoon.css";
-import "./css/modes/evening.css";
-import "./css/modes/night.css";
-import "./css/modes/rainbow.css";
+// import "./css/modes/morning.css";
+// import "./css/modes/afternoon.css";
+// import "./css/modes/evening.css";
+// import "./css/modes/night.css";
+// import "./css/modes/rainbow.css";
 
 function App() {
   return (
